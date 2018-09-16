9349dd68ac0f6ae8809656b892be3b764ee41695
Ika <ikatyang@gmail.com>
2018-09-16 17:02:50 +0800

    chore: update infra (#256)

--- src/prettierRule.ts
+++ src/prettierRule.ts
@@ -5,25 +5,23 @@ import * as prettier from 'prettier';
 import * as tslint from 'tslint';
 import * as ts from 'typescript';
 
-// tslint:disable:max-classes-per-file no-use-before-declare
-
 export class Rule extends tslint.Rules.AbstractRule {
-  public apply(source_file: ts.SourceFile): tslint.RuleFailure[] {
+  public apply(sourceFile: ts.SourceFile): tslint.RuleFailure[] {
     return this.applyWithWalker(
-      new Walker(source_file, this.ruleName, this.ruleArguments),
+      new Walker(sourceFile, this.ruleName, this.ruleArguments),
     );
   }
 }
 
 class Walker extends tslint.AbstractWalker<any[]> {
-  public walk(source_file: ts.SourceFile) {
-    const [rule_argument_1] = this.options;
+  public walk(sourceFile: ts.SourceFile) {
+    const [ruleArgument1] = this.options;
 
     let options: prettier.Options = {};
 
-    switch (typeof rule_argument_1) {
+    switch (typeof ruleArgument1) {
       case 'object':
-        options = rule_argument_1 as prettier.Options;
+        options = ruleArgument1 as prettier.Options;
         break;
       case 'string': {
         try {
@@ -37,18 +35,15 @@ class Walker extends tslint.AbstractWalker<any[]> {
           );
         }
 
-        const file_path = path.resolve(
-          process.cwd(),
-          rule_argument_1 as string,
-        );
-        const resolved_config = prettier.resolveConfig.sync(file_path);
+        const filePath = path.resolve(process.cwd(), ruleArgument1 as string);
+        const resolvedConfig = prettier.resolveConfig.sync(filePath);
 
         // istanbul ignore next
-        if (resolved_config === null) {
-          throw new Error(`Config file not found: ${file_path}`);
+        if (resolvedConfig === null) {
+          throw new Error(`Config file not found: ${filePath}`);
         }
 
-        options = resolved_config;
+        options = resolvedConfig;
         break;
       }
       default: {
@@ -60,19 +55,17 @@ class Walker extends tslint.AbstractWalker<any[]> {
           break;
         }
 
-        const resolved_config = prettier.resolveConfig.sync(
-          source_file.fileName,
-        );
+        const resolvedConfig = prettier.resolveConfig.sync(sourceFile.fileName);
 
-        if (resolved_config !== null) {
-          options = resolved_config;
+        if (resolvedConfig !== null) {
+          options = resolvedConfig;
         }
 
         break;
       }
     }
 
-    const source = source_file.getFullText();
+    const source = sourceFile.getFullText();
     const formatted = prettier.format(source, {
       parser: 'typescript',
       ...options,
@@ -86,28 +79,28 @@ class Walker extends tslint.AbstractWalker<any[]> {
       const {
         operation,
         offset: start,
-        deleteText: delete_text = '',
-        insertText: insert_text = '',
+        deleteText = '',
+        insertText = '',
       } = difference;
 
-      const end = start + delete_text.length;
-      const delete_code = utils.showInvisibles(delete_text);
-      const insert_code = utils.showInvisibles(insert_text);
+      const end = start + deleteText.length;
+      const deleteCode = utils.showInvisibles(deleteText);
+      const insertCode = utils.showInvisibles(insertText);
 
       switch (operation) {
         case 'insert':
           this.addFailureAt(
             start,
             1,
-            `Insert \`${insert_code}\``,
-            tslint.Replacement.appendText(start, insert_text),
+            `Insert \`${insertCode}\``,
+            tslint.Replacement.appendText(start, insertText),
           );
           break;
         case 'delete':
           this.addFailure(
             start,
             end,
-            `Delete \`${delete_code}\``,
+            `Delete \`${deleteCode}\``,
             tslint.Replacement.deleteFromTo(start, end),
           );
           break;
@@ -115,8 +108,8 @@ class Walker extends tslint.AbstractWalker<any[]> {
           this.addFailure(
             start,
             end,
-            `Replace \`${delete_code}\` with \`${insert_code}\``,
-            tslint.Replacement.replaceFromTo(start, end, insert_text),
+            `Replace \`${deleteCode}\` with \`${insertCode}\``,
+            tslint.Replacement.replaceFromTo(start, end, insertText),
           );
           break;
         // istanbul ignore next
@@ -128,6 +121,5 @@ class Walker extends tslint.AbstractWalker<any[]> {
 }
 
 function assert_existence_of_resolve_config_sync() {
-  // tslint:disable-next-line:strict-type-predicates
   assert(typeof prettier.resolveConfig.sync === 'function');
 }
